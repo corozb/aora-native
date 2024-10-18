a92df60c903e0100de18da35a55a5529c1da52bc
Cristian Orozco <cristianorozco.ben@gmail.com>
2024-10-18 16:56:46 -0500

    onboarding page

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,8 @@
 import { StatusBar } from "expo-status-bar";
-import { View, Text } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import { Link } from "expo-router";
 import { NativeWindStyleSheet } from "nativewind";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 NativeWindStyleSheet.setOutput({
   default: "native",
@@ -9,12 +10,17 @@ NativeWindStyleSheet.setOutput({
 
 export default function App() {
   return (
-    <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl color-primary font-pblack">Starter Kit</Text>
-      <StatusBar style="auto" />
-      <Link href="/home" style={{ color: "blue" }}>
-        Go to Home
-      </Link>
-    </View>
+    <SafeAreaView className="h-full">
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className="flex-1 items-center justify-center bg-white">
+          <Text className="text-3xl color-primary font-pblack">Starter Kit</Text>
+          <Text>On boarding page</Text>
+          <StatusBar style="auto" />
+          <Link href="/sign-in" style={{ color: "blue" }}>
+            Go to SignIn
+          </Link>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
